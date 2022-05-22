4cd965ce3512c2ea4bbed37585e09ec919bd49b1
Bas Kaal <3askaal@gmail.com>
2022-05-22 17:08:44 +0200

    clean up, add link to db issue fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,39 +1,26 @@
 const parse = require("pg-connection-string").parse;
 
+// https://forum.strapi.io/t/getting-strapi-working-on-digitalocean-app-plattform/13244/5
+
 const { host, port, database, user, password } = parse(
   process.env.DATABASE_URL
 );
 
-module.exports = ({ env }) => {
-  console.log('########### DB PROD ENV VARS ###########')
-  console.log({
-    host: env('DATABASE_HOST', '127.0.0.1'),
-    port: env.int('DATABASE_PORT', 5432),
-    database: env('DATABASE_NAME', 'strapi'),
-    user: env('DATABASE_USERNAME', 'strapi'),
-    password: env('DATABASE_PASSWORD', 'strapi'),
-    schema: env('DATABASE_SCHEMA', 'public'), // Not required
-    ssl: {
-      ca: env('DATABASE_CA')
-    },
-  });
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        schema: env('DATABASE_SCHEMA', 'public'),
-        ssl: {
-          ca: env('DATABASE_CA'),
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-        },
+      host,
+      port,
+      database,
+      user,
+      password,
+      schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: {
+        ca: env('DATABASE_CA'),
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
-      debug: false,
     },
+    debug: false,
   }
-};
+});
